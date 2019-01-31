5a02600abda2efa445920c0297166b6ea9aa9ce6
Roberto Cantore <rcantore@gmail.com>
2019-01-31 20:09:05 -0300

    Changed entities

--- core/src/main/java/com/rentacar/core/entities/Status.java
+++ core/src/main/java/com/rentacar/core/entities/Status.java
@@ -7,8 +7,13 @@ public class Status extends AbstractEntity {
     public static String STATUS_PENDING = "Pending";
     public static String STATUS_ON_TRIP = "On trip";
     public static String STATUS_TRIP_FINISHED = "Trip finished";
+    public static String STATUS_ERROR = "Error";
 
     private String description;
+
+    public Status() {
+
+    }
 
     public Status(String description) {
         this.description = description;
